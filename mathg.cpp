271e14433ac7dd67bf21d8701e6a7798a5b1e3d6
Meta-chan <k.sovailo@gmail.com>
2020-07-20 22:15:40 +0300

    Comparison with ir::MathC added

--- mathg.cpp
+++ mathg.cpp
@@ -9,23 +9,67 @@
 */
 
 #define IR_IMPLEMENT
+//#define IR_MATHC_OPENMP
 #include "mathg.h"
+#include <ir_math/ir_mathc.h>
+#include <random>
+#include <time.h>
+
+#define SIZE 1000
+#define N 100
+#define ALIGN 8
+
+void test()
+{
+	std::default_random_engine generator;
+	std::uniform_real_distribution<float> distribution(-1.0f, 1.0f);
+	bool ok;
+	
+	ir::MatrixC<float, ALIGN> ac(SIZE, SIZE, &ok); if (!ok) return;
+	for (unsigned int i = 0; i < SIZE; i++)
+	{
+		for (unsigned int j = 0; j < SIZE; j++)	ac.data(i)[j] = distribution(generator);
+	}
+	MatrixG ag(SIZE, SIZE, &ok); if (!ok) return;
+	for (unsigned int i = 0; i < SIZE; i++)
+	{
+		if (!ag.store_line(i, ac.data(i))) return;
+	}
+
+	ir::VectorC<float, ALIGN> bc(SIZE, &ok); if (!ok) return;
+	for (unsigned int i = 0; i < SIZE; i++)	bc.data()[i] = distribution(generator);
+	VectorG bg(SIZE, &ok); if (!ok) return;
+	if (!bg.store(bc.data())) return;
+
+	ir::VectorC<float, ALIGN> cc(SIZE, &ok); if (!ok) return;
+	VectorG cg(SIZE, &ok); if (!ok) return;
+	
+	ir::MathC<float, ALIGN>::multiply_mvv(&ac, &bc, &cc);
+	clock_t cl = clock();
+	for (unsigned int i = 0; i < N; i++)
+	{
+		ir::MathC<float, ALIGN>::multiply_mvv(&ac, &bc, &cc);
+	}
+	printf("CPU done in %f seconds\n", (float)(clock() - cl)/CLOCKS_PER_SEC);
+
+	MathG::multiply_mvv(&ag, &bg, &cg);
+	cl = clock();
+	for (unsigned int i = 0; i < N; i++)
+	{
+		MathG::multiply_mvv(&ag, &bg, &cg);
+	}
+	printf("GPU done in %f seconds\n", (float)(clock() - cl) / CLOCKS_PER_SEC);
+
+	cg.load(cc.data());
+	return;
+};
 
 int main()
 {
 	if (MathG::init(true))
 	{
-		bool ok;
-		MatrixG a(2, 2, &ok);
-		VectorG b(2, &ok);
-		VectorG c(2, &ok);
-		float adata[4] = { 0.25f, 0.5f, 0.75f, 1.0f };
-		a.store(adata);
-		float bdata[2] = { 0.5f, 1.0f };
-		b.store(bdata);
-		MathG::multiply_mvv(&a, &b, &c);
-		float cdata[2];
-		c.load(cdata);
+		test();
 		MathG::free();
 	}
+	getchar();
 };
